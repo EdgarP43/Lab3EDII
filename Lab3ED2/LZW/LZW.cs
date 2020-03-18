33b9ca6898cf66ed0a8b54637bbcf4e66bae7989
EdgarP43 <e.paredes-cas@outlook.com>
2020-03-18 12:44:12 -0600

    Diccionario LZW

---
+++ Lab3ED2/LZW/LZW.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab3ED2.LZW
+{
+    public class LZW
+    {
+        const int bufferLength = 1000000;
+        static public Dictionary<string, int> ObtenerDiccionarioCaracteresEspeciales(string RutaOriginal, ref int indice)
+        {
+            var DiccionarioAEscribir = new Dictionary<string, int>();
+            using (var stream = new FileStream(RutaOriginal, FileMode.Open))
+            {
+                using (var reader = new BinaryReader(stream))
+                {
+                    var byteBuffer = new byte[bufferLength];
+
+                    while (reader.BaseStream.Position != reader.BaseStream.Length)
+                    {
+                        byteBuffer = reader.ReadBytes(bufferLength);
+                        for (int i = 0; i < byteBuffer.Length; i++)
+                        {
+                            var llave = Convert.ToString(Convert.ToChar(byteBuffer[i]));
+                            if (!DiccionarioAEscribir.ContainsKey(llave))
+                            {
+                                DiccionarioAEscribir.Add(llave, indice);
+                                indice++;
+                            }
+                        }
+                    }
+                }
+            }
+            return DiccionarioAEscribir;
+        }
+    }
+}
