691d2428fe09f261225671e3b08b423e7f49809e
Cristian <cool232777@gmail.com>
2020-03-20 21:34:28 -0600

    Creacion API para algoritmo LZW terminado

--- Lab3ED2/Controllers/ValuesController.cs
+++ Lab3ED2/Controllers/ValuesController.cs
@@ -78,7 +78,7 @@ namespace Lab3ED2.Controllers
 
                     Huffman.Huffman.Instancia.DatosDeArchivos.Add(Archivo.NombreArchivo, Archivo);
 
-                    listaComprimidos.Add(file.FileName + ".huff");
+                    listaComprimidos.Add(nombreArchivo + ".huff");
                 }
             }
         }
@@ -103,7 +103,67 @@ namespace Lab3ED2.Controllers
             {
                 var g = "";
             }
+        }
 
+        [Route("Compresionlzw")]
+        [HttpPost]
+        public void CompresiónLZW([FromForm] IFormFile file)
+        {
+            var nombreArchivo = file.FileName;
+            var nombre = nombreArchivo.Split('.');
+            nombreArchivo = nombre[0];
+            var disk = "D:\\";
+            var PesoOriginal = Convert.ToDouble(file.Length);
+            if (file != null && file.Length > 0)
+            {
+                var model = "";
+                model = Path.GetRelativePath("D:/", file.FileName);
+                var Model = "";
+                Model = disk + model;
+                var ubica = Path.GetFullPath("Archivos Comprimidos\\");
+                var UbicacionAAlmacenarLZW = "Archivos comprimidos\\" + file.FileName;
+                if (LZW.LZW.Comprimir(Model, nombre, ubica) == 1)
+                {
+                    var pathArchivoCompri = Path.GetFullPath("Archivos Comprimidos\\");
+                    var RutaArchivoCompreso = (pathArchivoCompri + nombreArchivo + ".lzw");
+
+
+                    var ArchivoCompreso = new FileInfo(RutaArchivoCompreso);
+                    var PesoCompreso = Convert.ToDouble(ArchivoCompreso.Length);
+
+                    var Archivo = new Archivo();
+                    Archivo.NombreArchivo = nombreArchivo;
+                    Archivo.Factor = Math.Round(PesoOriginal / PesoCompreso, 3);
+                    Archivo.Razon = Math.Round(PesoCompreso / PesoOriginal, 3);
+                    Archivo.Porcentaje = Math.Round(100 * (1 - Convert.ToDouble(Archivo.Razon)), 3);
+
+                    Huffman.Huffman.Instancia.DatosDeArchivos.Add(Archivo.NombreArchivo, Archivo);
+
+                    listaComprimidos.Add(nombreArchivo + ".lzw");
+                }
+            }
+        }
+
+        [Route("Descompresionlzw")]
+        [HttpPost]
+        public void DescompresiónLZW([FromForm] IFormFile Nombre)
+        {
+            var NombreArchivo = Nombre.FileName;
+            var nombre = NombreArchivo.Split('.');
+            var nombreArchivo = nombre[0];
+            var filePath = Path.GetFullPath("Archivos Comprimidos\\");
+            filePath = filePath + NombreArchivo;
+            var pathDescompress = Path.GetFullPath("Archivos Descomprimidos\\");
+            var Archivo = new FileStream(filePath, FileMode.Open);
+            Archivo.Close();
+            var model = filePath;
+
+            var UbicacionDescomprimidos = pathDescompress;
+
+            if (LZW.LZW.Descomprimir(model, nombre, UbicacionDescomprimidos) == 1)
+            {
+                var g = "";
+            }
         }
     }
 }
